f6ef2a3204aaa31fa05f2ca619ce564984860c20
hanheel <hanhee630630@gmail.com>
2024-07-05 01:16:06 +0900

    App.jsx commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import styles from "./App.module.css";
 import Header from "./Components/Header/Header";
-// import PortfolioWrite from "./Screen/MyPage/MyPortfolio/PortfolioWrite/PortfolioWrite";
-// import ApplyProfile from "./Screen/MyProject/HiringProj/Apply_Profile/ApplyProfile";
- import MyPortfolio from "./Screen/MyPage/MyPortfolio/MyPortfolio";
+import PortfolioWrite from "./Screen/MyPage/MyPortfolio/PortfolioWrite/PortfolioWrite";
+//import ApplyProfile from "./Screen/MyProject/ApplyHireProj/ApplyProfile/ApplyProfile";
+//import MyPortfolio from "./Screen/MyPage/MyPortfolio/MyPortfolio";
 //import MyProfile from "./Screen/MyPage/MyProfile/MyProfile";
-// import OngoingProj from "./Screen/MyProject/OngoingProj/OngoingProj";
+ //import OngoingProj from "./Screen/MyProject/AttendProj/AttendProj";
 
 function App() {
   return (
   <div className={styles.wrap}>
     <Header />
     <div className={styles.main}> 
-    <MyPortfolio /> {/*라우팅 시 outlet이 들어갈 부분*/} 
+    <PortfolioWrite /> {/*라우팅 시 outlet이 들어갈 부분*/} 
     </div>
   </div>
   )
