4f61da6d736331ca8b9a9b957d61649ac73ef420
hanheel <hanhee630630@gmail.com>
2024-07-04 22:32:03 +0900

    pull : pull한 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import styles from "./App.module.css";
 import Header from "./Components/Header/Header";
- import PortfolioWrite from "./Screen/MyPage/MyPortfolio/PortfolioWrite/PortfolioWrite";
+// import PortfolioWrite from "./Screen/MyPage/MyPortfolio/PortfolioWrite/PortfolioWrite";
 // import ApplyProfile from "./Screen/MyProject/HiringProj/Apply_Profile/ApplyProfile";
 // import MyPortfolio from "./Screen/MyPage/MyPortfolio/MyPortfolio";
-//import MyProfile from "./Screen/MyPage/MyProfile/MyProfile";
-import OngoingProj from "./Screen/MyProject/OngoingProj/OngoingProj";
+import MyProfile from "./Screen/MyPage/MyProfile/MyProfile";
+// import OngoingProj from "./Screen/MyProject/OngoingProj/OngoingProj";
 
 function App() {
   return (
   <div className={styles.wrap}>
     <Header />
     <div className={styles.main}> 
-    <OngoingProj/> {/*라우팅 시 outlet이 들어갈 부분*/} 
+    <MyProfile/> {/*라우팅 시 outlet이 들어갈 부분*/} 
     </div>
   </div>
   )
