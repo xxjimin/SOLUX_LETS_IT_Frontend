21d1e0bb7fd84c9af9085ce1df7e3c92506f383e
hanheel <hanhee630630@gmail.com>
2024-07-07 00:17:16 +0900

    브랜치 전환 위해 push

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import styles from "./App.module.css";
 import Header from "./Components/Header/Header";
-import PortfolioWrite from "./Screen/MyPage/MyPortfolio/PortfolioWrite/PortfolioWrite";
+import ApplyHireProj from "./Screen/MyProject/ApplyHireProj/ApplyHireProj";
+//import CompletePort from "./Screen/MyPage/MyPortfolio/PortfolioWrite/Complete/CompletePort";
+//import PortfolioWrite from "./Screen/MyPage/MyPortfolio/PortfolioWrite/PortfolioWrite";
 //import ApplyProfile from "./Screen/MyProject/ApplyHireProj/ApplyProfile/ApplyProfile";
 //import MyPortfolio from "./Screen/MyPage/MyPortfolio/MyPortfolio";
 //import MyProfile from "./Screen/MyPage/MyProfile/MyProfile";
- //import OngoingProj from "./Screen/MyProject/AttendProj/AttendProj";
+ //import AttendProj from "./Screen/MyProject/AttendProj/AttendProj";
+//import Scrap from "./Screen/MyProject/Scrap/Scrap";
 
 function App() {
   return (
   <div className={styles.wrap}>
     <Header />
     <div className={styles.main}> 
-    <PortfolioWrite /> {/*라우팅 시 outlet이 들어갈 부분*/} 
+    <ApplyHireProj /> {/*라우팅 시 outlet이 들어갈 부분*/} 
     </div>
   </div>
   )
