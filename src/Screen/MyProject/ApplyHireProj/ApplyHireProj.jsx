d3aed0c323976035a60e81709be1d32d08c9ef94
hanheel <hanhee630630@gmail.com>
2024-07-07 03:06:20 +0900

    screen : 구인/신청 프로젝트 화면 구현 완료

--- src/Screen/MyProject/ApplyHireProj/ApplyHireProj.jsx
+++ src/Screen/MyProject/ApplyHireProj/ApplyHireProj.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react"
 import Button from "../../../Components/Button/Button"
 import RouteName from "../../../Components/RouteName/RouteName"
 import SideNav from "../../../Components/SideNav/SideNav"
 import ApplicantBtn from "./ApplicantBtn/ApplicantBtn"
 import styles from './ApplyHireProj.module.css'
+import MemberView from "./MemberView/MemberView"
 
 const route = ['내 프로젝트','구인/신청 프로젝트']
 const sidenavCont = ['구인/신청 프로젝트','참여 프로젝트','스크랩']
 
+
+
 const ApplyHireProj = ()=>{
+    //클릭하면 팀원을 보여주도록 state 설정
+    const [showMember, setShowMember] = useState(false)
+    const clickMember = ()=> {setShowMember(!showMember)}
+    console.log(showMember)
+
     return(
         <div className={styles.ApplyHireProj}>
             <RouteName route={route} />
@@ -37,14 +46,12 @@ const ApplyHireProj = ()=>{
                                     <div className='Project_Component' style={{width:'315px', height:'240px',backgroundColor:'var(--select-color)'}} />
                                     <div className={styles.HireProj__buttonWrap}>
                                         <Button text='모집 마감'/>
-                                        <Button text='팀원' type='SEC_70x40'/>
+                                        <Button text='팀원' type='SEC_70x40' onClick={clickMember}/>
                                         <Button  text='글 삭제' type='NONE__TEXT-TC2'/>
                                     </div>
 
-                                    {/*프로젝트 팀원 */}
-                                    <div className={styles.HireProj__teamMate}>
-                                    
-                                    </div>
+                                    {/*프로젝트 팀원창 */}   
+                                    {showMember && <MemberView />}
                                 </div>
 
                                 {/*프로젝트 신청자*/}
@@ -65,11 +72,13 @@ const ApplyHireProj = ()=>{
                             </div>
                             {/*프로젝트 구인글 + 버튼 + 신청자 목록 (컨텐츠) */}
                             <div className={styles.ApplyProj__content}>
-                                <div className='Project_Component' style={{width:'315px', height:'240px',backgroundColor:'var(--select-color)'}} />
-                                <div className='Project_Component' style={{width:'315px', height:'240px',backgroundColor:'var(--select-color)'}} />
-                                <div className='Project_Component' style={{width:'315px', height:'240px',backgroundColor:'var(--select-color)'}} />
-                                <div className='Project_Component' style={{width:'315px', height:'240px',backgroundColor:'var(--select-color)'}} />
-                                <div className='Project_Component' style={{width:'315px', height:'240px',backgroundColor:'var(--select-color)'}} />
+                                <div className={styles.ApplyProj__ProjectList}>
+                                    <div className='Project_Component' style={{width:'315px', height:'240px',backgroundColor:'var(--select-color)'}} />
+                                    <div className='Project_Component' style={{width:'315px', height:'240px',backgroundColor:'var(--select-color)'}} />
+                                    <div className='Project_Component' style={{width:'315px', height:'240px',backgroundColor:'var(--select-color)'}} />
+                                    <div className='Project_Component' style={{width:'315px', height:'240px',backgroundColor:'var(--select-color)'}} />
+                                    <div className='Project_Component' style={{width:'315px', height:'240px',backgroundColor:'var(--select-color)'}} />
+                                </div>
                             </div>
                     </div>
                     </div>
