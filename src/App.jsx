b629b83325e3adee6bdffe3c3b80a77cf1fa5d93
hanheel <hanhee630630@gmail.com>
2024-07-07 22:42:36 +0900

    checkout 위해서 push

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import styles from "./App.module.css";
 import Header from "./Components/Header/Header";
-import ApplyHireProj from "./Screen/MyProject/ApplyHireProj/ApplyHireProj";
+//import ApplyHireProj from "./Screen/MyProject/ApplyHireProj/ApplyHireProj";
 //import CompletePort from "./Screen/MyPage/MyPortfolio/PortfolioWrite/Complete/CompletePort";
 //import PortfolioWrite from "./Screen/MyPage/MyPortfolio/PortfolioWrite/PortfolioWrite";
-//import ApplyProfile from "./Screen/MyProject/ApplyHireProj/ApplyProfile/ApplyProfile";
+import ApplyProfile from "./Screen/MyProject/ApplyHireProj/ApplyProfile/ApplyProfile";
 //import MyPortfolio from "./Screen/MyPage/MyPortfolio/MyPortfolio";
 //import MyProfile from "./Screen/MyPage/MyProfile/MyProfile";
- //import AttendProj from "./Screen/MyProject/AttendProj/AttendProj";
+// import AttendProj from "./Screen/MyProject/AttendProj/AttendProj";
 //import Scrap from "./Screen/MyProject/Scrap/Scrap";
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
   <div className={styles.wrap}>
     <Header />
     <div className={styles.main}> 
-    <ApplyHireProj /> {/*라우팅 시 outlet이 들어갈 부분*/} 
+    <ApplyProfile /> {/*라우팅 시 outlet이 들어갈 부분*/} 
     </div>
   </div>
   )
